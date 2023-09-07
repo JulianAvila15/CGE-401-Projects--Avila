2141ca5a56256439a66c21a22713f6a491b8f91e
JulianAvila15 <123836061+JulianAvila15@users.noreply.github.com>
2023-09-07 15:19:30 -0500

    Changes Made on 9/7/2023

---
+++ Prototype1/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class ScoreManager : MonoBehaviour
+{
+    public static bool gameOver = false;
+    public static bool won = false;
+    public static int score = 0;
+
+    public Text textbox;
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if game is not over display score
+        if (!gameOver)
+        {
+            textbox.text = "Score: " + score;
+        }
+
+        //win condition: score 3 points
+        if(score>=3)
+        {
+            won = true;
+            gameOver = true;
+        }
+
+        if(gameOver)
+        {
+            if (won)
+            {
+                textbox.text = "You win!\n Press R to try again!";
+            }
+            else
+            {
+                textbox.text = "You lose!\n Press R to try again!";
+            }
+            if(Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+}
