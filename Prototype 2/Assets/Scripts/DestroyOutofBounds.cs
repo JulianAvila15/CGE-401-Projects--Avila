05faf4f4fe76031f1828c9b3a05f6256af5a9210
JulianAvila15 <123836061+JulianAvila15@users.noreply.github.com>
2023-09-16 17:03:45 -0500

    Prototype 2 Final Changes (9/16/2023)

---
+++ Prototype 2/Assets/Scripts/DestroyOutofBounds.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Julian Avila
+ * Prototype 2
+ * Destroys objects when they reach an out of bounds position
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutofBounds : MonoBehaviour
+{
+    public float topBound = 20, bottomBound = -10;
+    private HealthSystem healthSystemScript;
+
+    void Start()
+    {
+        healthSystemScript = GameObject.FindGameObjectWithTag("HealthSystem").GetComponent<HealthSystem>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if(transform.position.z<bottomBound)
+        {
+            //grab health sys script and call the take damage method
+            healthSystemScript.TakeDamage();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Prototype 2/Assets/Scripts/DetectCollisions.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Julian Avila
+ * Prototype 2
+ * Detects collisions with other objects
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//attach to food prefab
+public class DetectCollisions : MonoBehaviour
+{
+    private DisplayScore displayScoreScript;
+   
+    void Start()
+    {
+        displayScoreScript = GameObject.FindGameObjectWithTag("DisplayScoreText").GetComponent<DisplayScore>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        displayScoreScript.score++;
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+        
+    }
+}
