c35d406abc64d75341c366bbdda589309f588acb
JulianAvila15 <123836061+JulianAvila15@users.noreply.github.com>
2023-12-01 19:17:55 -0600

    Prototype 8 finished

---
+++ Prototype 8/Assets/Scripts/GameManager.cs
@@ -0,0 +1,69 @@
+﻿/*Julian Avila
+ * Prototype 8
+ *Manages game events such as game over and score
+ */
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    private float spawnRate= 1.0f;
+    public TextMeshProUGUI scoreText,gameOverText;
+    private int score;
+
+    public bool isGameActive;
+
+    public GameObject restartButton,titleScreen;
+    
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(score);
+        restartButton.gameObject.SetActive(false);
+        titleScreen.SetActive(false);
+        spawnRate /= difficulty;
+    }
+
+   public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    IEnumerator SpawnTarget()
+    {
+       while(isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+
+            int index = UnityEngine.Random.Range(0, targets.Count);
+            
+            //spawn the prefab at the randomly selected index
+            Instantiate(targets[index]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
---
+++ Prototype 8/Assets/Scripts/Target.cs
@@ -0,0 +1,81 @@
+﻿/*
+ * Julian Avila
+ * Prototype 8
+ * Target script for target objects, gives them physics and health
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRB;
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawnPos = -6;
+
+    private GameManager gameManager;
+
+    public ParticleSystem explosionParticle;
+
+    public int pointValue=0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRB = GetComponent<Rigidbody>();
+
+        //Add force upwards multiplied by a randomized speed
+        targetRB.AddForce(RandomForce(), ForceMode.Impulse);
+
+        //Add a random torque (rotational force) with a randomized 
+        targetRB.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+
+        //set the position with a randomized x value
+        transform.position = RandomPosition();
+
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    }
+
+    private Vector3 RandomPosition()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+
+    private Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+   private float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    private void OnMouseDown()
+    {
+        if (gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            gameManager.UpdateScore(pointValue);
+            Instantiate(explosionParticle, transform.position, transform.rotation);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(gameObject.CompareTag("Red"))
+        {
+            gameManager.GameOver();
+        }
+
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
